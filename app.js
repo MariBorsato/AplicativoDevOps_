0bb164ed8333cc58a887b2a826d95025fe62a419
MariBorsato <mariana.759493@graduacao.fsa.br>
2024-05-17 21:53:00 -0300

    Create app.js
    Criação do arquivo app.js que verifica se um número é par ou ímpar

---
+++ app.js
@@ -0,0 +1,11 @@
+// Definindo ua lista de números
+let numeros = [2,4,6,8,10]
+// Percorrendo a lista com um loop for
+for(let i = 0; i < numeros.length; i++){
+// Verificando se o número é par ou ímpar
+  if(nuemro[i] % 2 === 0)[
+    console.log(numero[i] = " é um numero par);
+                }else{
+  console.long(numero[i] + " é um numero ímpar);
+               {
+} 
